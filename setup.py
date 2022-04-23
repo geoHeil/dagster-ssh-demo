761788d0e72b9c539a3541fc49fbbb40778ac8e2
Georg Heiler <georg.kf.heiler@gmail.com>
2022-04-23 07:54:11 +0200

    upgrade version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-dagster_version = "0.14.10"
+dagster_version = "0.14.12"
 setuptools.setup(
     name="SSH_DEMO",
     packages=setuptools.find_packages(exclude=["SSH_DEMO_tests"]),
